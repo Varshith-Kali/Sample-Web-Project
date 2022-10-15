4d636105a438e279d68228ea8a0f3f467da5043c
Varshith <106383116+Varshith-Kali@users.noreply.github.com>
2022-10-15 19:24:33 +0530

    Create html.v
    - Use a proper compiler to run the the html code.
    - It gives the output as a pic of a pizza and gives you the option to add the toppings on it.

---
+++ html.v
@@ -0,0 +1,22 @@
+<html>
+ <head>
+ </head>
+ <body>
+    <h3> Pizza : </h3>
+     <button type="button" onclick=>Add Toppings</button> 
+     <ul>
+        <li>Pepperoni.</li>
+        <li>Sausage.</li>
+        <li>Chicken.</li>
+        <li>Bacon.</li>
+        <li>Onions.</li>
+        <li>Mushroom.</li>
+        <li>Pepper.</li>
+        <li>Extra Cheese.</li>
+</ul>
+
+    <img
+     src="https://st.depositphotos.com/1105977/1595/i/950/depositphotos_15951557-stock-photo-fresh-pizza.jpg" width = "500">
+     
+ </body>
+</html>
